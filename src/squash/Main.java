98dac5150a173413450a37315cdb05343cf8ddbf
Yevhenii Kukhol <yevhenii.kukhol@gmail.com>
2020-10-12 19:45:49 +0300

    msg2

--- src/squash/Main.java
+++ src/squash/Main.java
@@ -3,9 +3,6 @@ package squash;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("lol kek");
-        System.out.println("lol kek1");
-        System.out.println("lol ke2");
-        System.out.println("lol ke3");
+	// write your code here
     }
 }
