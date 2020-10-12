b0ea39cb13782478c9b6a5c9063678fe86f362c5
Yevhenii Kukhol <yevhenii.kukhol@gmail.com>
2020-10-12 19:04:11 +0300

    super commit
    commit 3

--- src/squash/Main.java
+++ src/squash/Main.java
@@ -3,6 +3,9 @@ package squash;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        System.out.println("lol kek");
+        System.out.println("lol kek1");
+        System.out.println("lol ke2");
+        System.out.println("lol ke3");
     }
 }
